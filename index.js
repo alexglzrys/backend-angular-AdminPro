b887e0d3dc9f0ccfe1cf8ab34fc6f5fd3fe97001
Alejandro González Reyes <alexglzrys@gmail.com>
2022-01-02 13:34:54 -0600

    crear primera ruta

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const app = express();
 
+// rutas
+app.get('/', (req, res) => {
+    res.status(200).json({
+        ok: true,
+        msg: 'Hola mundo'
+    });
+});
+
 app.listen(3000, () => {
     console.log('Servidor corriendo en el puerto 3000');
 });
