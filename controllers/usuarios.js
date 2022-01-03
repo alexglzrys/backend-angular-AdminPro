00bff2cbaffb45f2f48fc867dc460d294d4792d0
Alejandro González Reyes <alexglzrys@gmail.com>
2022-01-02 18:29:44 -0600

    validar email único

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -12,14 +12,33 @@ const getUsuarios = async(req = request, res = response) => {
 
 const crearUsuario = async(req = request, res = response) => {
     const { email, password, nombre } = req.body;
-    // Guardar usuario en base de datos
-    const usuario = new Usuario(req.body);
-    await usuario.save();
 
-    res.status(200).json({
-        ok: true,
-        usuario
-    });
+    try {
+        // Verificar duplicidad de email
+        const existeEmail = await Usuario.findOne({email});
+        console.log(existeEmail);
+        if (existeEmail) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El correo ya se encuentra en uso por otro usuario'
+            });
+        }
+
+        // Guardar usuario en base de datos
+        const usuario = new Usuario(req.body);
+        await usuario.save();
+
+        res.status(200).json({
+            ok: true,
+            usuario
+        });
+    } catch (err) {
+        res.status(500).json({
+            ok: false,
+            msg: 'Error interno en el servidor'
+        });
+    }
+    
 }
 
 module.exports = {
