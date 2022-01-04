181c63a0cf65d48f104253c4729c06bf73e0656f
Alejandro González Reyes <alexglzrys@gmail.com>
2022-01-04 09:37:11 -0600

    lógica buscador general de forma simultanea

--- controllers/buscador.js
+++ controllers/buscador.js
@@ -1,11 +1,36 @@
 const { request, response } = require("express");
+const Hospital = require("../models/Hospital");
+const Medico = require("../models/Medico");
+const Usuario = require("../models/Usuario");
 
-const buscadorGeneral = (req = request, res = response) => {
+const buscadorGeneral = async(req = request, res = response) => {
     const busqueda = req.params.busqueda;
+
+    // Expresion regular: LIKE
+    const regexBusqueda = new RegExp(busqueda, 'i');
+
+    // Optimizar tareas asincronas (no hay dependencia entre ellas)
+    const [usuarios, medicos, hospitales] = await Promise.all([
+        // Buscar usuarios
+        Usuario.find({ nombre: regexBusqueda }),
+        // Buscar médicos
+        Medico.find({ nombre: regexBusqueda }),
+        // Buscar hospitales
+        Hospital.find({ nombre: regexBusqueda} )
+    ]);
+
+    // Buscar usuarios
+    // const usuarios = await Usuario.find({ nombre: regexBusqueda });
+    // Buscar médicos
+    // const medicos = await Medico.find({ nombre: regexBusqueda });
+    // Buscar hospitales
+    // const hospitales = await Hospital.find({ nombre: regexBusqueda} )
 
     res.status(200).json({
         ok: true,
-        msg: 'buscador general',
+        usuarios,
+        medicos,
+        hospitales,
         busqueda
     });
 }
