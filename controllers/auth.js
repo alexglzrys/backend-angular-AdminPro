1a19bd55b6d549d793e9ad72da942b96860c8444
Alejandro González Reyes <alexglzrys@gmail.com>
2022-01-05 18:29:24 -0600

    registrar usuarios logeados por google

--- controllers/auth.js
+++ controllers/auth.js
@@ -49,12 +49,38 @@ const googleSignIn = async(req = request, res = response) => {
         // Verificar token de google
         const {name, email, picture } = await googleVerify(googleToken);
 
+        myUser = null;
+
+        // Buscar el usuario en la base de datos
+        const usuarioDB = await Usuario.findOne({ email });
+        if (!usuarioDB) {
+            // Registrar el nuevo usuario con credenciales de Google
+            myUser = new Usuario({
+                nombre: name,
+                email,
+                // Colocamos un pass fake, dado que el modelo exige esta info
+                // Esto no quiere decir que se puede logear con este pass fake, ya que no esta hasheado
+                password: 'usuario@google', 
+                img: picture,
+                google: true
+            })
+        } else {
+            // Usuario existe, es decir ya se registro previamente de forma manual (email / password)
+            // Quizá ya había accedido previamente mediante autenticación de google
+            myUser = usuarioDB;
+            myUser.google = true;
+        }
+
+        // Guardar el usuario en base de datos.
+        // Para usuarios logeados anteriormente, se actualiza su registro a acceso con Google = true
+        await myUser.save();
+
+        // Generar el Token JWT que le da acceso al usuario a mi App
+        const token = await generarJWT(myUser.id);
+
         res.status(200).json({
             ok: true,
-            msg: 'Google Sign in',
-            name,
-            email,
-            picture
+            token
         });
     } catch (err) {
         res.status(500).json({
